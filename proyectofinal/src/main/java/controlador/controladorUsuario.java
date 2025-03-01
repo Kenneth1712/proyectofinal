5a9820a7d9bf69dd3769b0f2d16c4d870b253fe1
kenneth1712 <kennethcuesta17122016@gmail.com>
2025-03-01 17:50:14 -0500

    cambio en rama

--- proyectofinal/src/main/java/controlador/controladorUsuario.java
+++ proyectofinal/src/main/java/controlador/controladorUsuario.java
@@ -1,5 +1,25 @@
 package controlador;
 
+import java.util.Date;
+import java.text.ParseException;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/ver	/e1")
 public class controladorUsuario {
+	
+
 
 }
